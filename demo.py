55fee512eb4c73cb1c9ad96bcb6e3835cf1724f5
Falanke21 <1065314944@qq.com>
2019-10-05 15:30:35 -0400

    initial commit

---
+++ demo.py
@@ -0,0 +1,13 @@
+# Demo file
+import time
+
+if __name__ == "__main__":
+	while True:
+		for i in range(40):
+			time.sleep(0.02)
+			print(" " * i, end="")
+			print("Ass we can")
+		for i in range(40, 0, -1):
+			time.sleep(0.02)
+			print(" " * i, end="")
+			print("Ass we can")
