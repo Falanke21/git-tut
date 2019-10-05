36887d3bf5e26d8e97e771e295719279f0cb89f8
dinoli <dino.li@mail.utoronto.ca>
2019-10-05 15:37:00 -0400

    Frank

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 		for i in range(40):
 			time.sleep(0.02)
 			print(" " * i, end="")
-			print("Ass we can")
+			print("Frank")
 		for i in range(40, 0, -1):
 			time.sleep(0.02)
 			print(" " * i, end="")
